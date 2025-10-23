4411941ef17956823dba7267d1a07460c5721bd2
Ostap Seniuk <ostap_seniuk@epam.com>
2025-10-24 00:00:36 +0300

    refactoring

--- src/main/java/org/ost/advertisement/repository/query/meta/SqlDtoFieldDefinition.java
+++ src/main/java/org/ost/advertisement/repository/query/meta/SqlDtoFieldDefinition.java
@@ -8,9 +8,8 @@ public record SqlDtoFieldDefinition<T>(
 	@Getter String dtoField,
 	@Getter String sqlField,
 	@Getter ValueExtractor<T> extractor
-) implements SqlDtoFieldExtractor<T> {
+) {
 
-	@Override
 	public T extract(ResultSet rs) throws SQLException {
 		return extractor.apply(rs, dtoField);
 	}
