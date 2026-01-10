f0be68c7139caf8819916d45d1fca7d11a71d2d2
OstMutant <ost.mutant@gmail.com>
2026-01-11 00:00:56 +0200

    refactoring

--- src/main/java/org/ost/advertisement/configuration/ui/AppShell.java
+++ src/main/java/org/ost/advertisement/configuration/ui/AppShell.java
@@ -2,10 +2,7 @@ package org.ost.advertisement.configuration.ui;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 
 @PWA(name = "Advertisement App", shortName = "AdvertApp")
-@Theme(themeClass = Lumo.class)
 public class AppShell implements AppShellConfigurator {
 }
