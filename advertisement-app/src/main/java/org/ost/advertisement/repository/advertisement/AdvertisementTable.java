337c7a992312c23b4b0d5d06032c7421e2f53d27
OstMutant <ost.mutant@gmail.com>
2026-04-18 18:33:44 +0300

    refactoring

--- advertisement-app/src/main/java/org/ost/advertisement/repository/advertisement/AdvertisementTable.java
+++ advertisement-app/src/main/java/org/ost/advertisement/repository/advertisement/AdvertisementTable.java
@@ -15,13 +15,18 @@ public final class AdvertisementTable {
     public static final String CREATED_AT  = ALIAS + ".created_at";
     public static final String UPDATED_AT  = ALIAS + ".updated_at";
 
-    public static final String MAIN_IMAGE_URL =
-            "(SELECT url FROM attachment WHERE entity_type = 'ADVERTISEMENT' AND entity_id = " + ALIAS + ".id " +
-            "ORDER BY created_at ASC LIMIT 1)";
+    public static final String MAIN_IMAGE_URL = "att.main_image_url";
+    public static final String IMAGE_COUNT    = "COALESCE(att.image_count, 0)";
 
-    public static final String IMAGE_COUNT =
-            "(SELECT COUNT(*) FROM attachment WHERE entity_type = 'ADVERTISEMENT' AND entity_id = " + ALIAS + ".id)";
+    private static final String ATTACHMENT_JOIN =
+            " LEFT JOIN (SELECT entity_id," +
+            " COUNT(*) AS image_count," +
+            " (array_agg(url ORDER BY created_at ASC))[1] AS main_image_url" +
+            " FROM attachment WHERE entity_type = 'ADVERTISEMENT' GROUP BY entity_id) att" +
+            " ON att.entity_id = " + ALIAS + ".id";
 
     public static final String SOURCE =
-            TABLE + " " + ALIAS + " LEFT JOIN user_information u ON " + ALIAS + ".created_by_user_id = u.id";
+            TABLE + " " + ALIAS +
+            " LEFT JOIN user_information u ON " + ALIAS + ".created_by_user_id = u.id" +
+            ATTACHMENT_JOIN;
 }
